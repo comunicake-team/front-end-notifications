94180f8295cd118c1d19ada77c9ec831a921ea2a
Dumitru Mitaru-Berceanu <du@sporkbytes.com>
2020-09-06 12:54:24 -0700

    Adds button to send text message to daniel.

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,16 +3,27 @@ import { Button } from '@material-ui/core';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useParams } from 'react-router-dom';
 import { Typography } from '@material-ui/core';
+import Axios from 'axios';
 
 const Profile = props => {
 	const { id } = useParams();
-	const { logout } = useAuth0();
+	const { logout, user } = useAuth0();
 
 	return (
 		<div>
 			<Typography variant="h2">
 				Suck My DIck Person with id of: {id}
 			</Typography>
+
+			<Button
+				variant="contained"
+				color="primary"
+				onClick={() =>
+					Axios.post(`http://localhost:4000/${user.email}/send-text`)
+				}
+			>
+				Send Mesagge
+			</Button>
 			<Button
 				variant="contained"
 				color="primary"
