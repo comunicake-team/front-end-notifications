2c34cdea4a4c857fc21c2f35ec31b80fe8996352
Daniel Hartnell <danielhartnell@gmail.com>
2020-09-12 18:16:21 -0700

    Provides a basic landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,16 +6,21 @@ import Page from '../components/Page';
 const LandingPage = () => {
 	return (
 		<Page>
-			<Typography variant="h3" gutterBottom>
-				Welcome to My FUcking App
+			<Typography variant="h2" gutterBottom>
+				Welcome to comunicake!
+			</Typography>
+			<Typography variant="h5" gutterBottom>
+				Simple text notifications
 			</Typography>
 			<Typography>
-				This is the description of the service we offer Lorem ipsum
-				dolor sit amet consectetur adipisicing elit. Molestias nihil
-				corporis odio vero a fugiat accusantium aspernatur nisi quasi,
-				iste quae magni tempora voluptas est autem doloribus, enim
-				dolorum ratione? SHES ALLIIIIIVEE
+				Comunicake is a simple, straightforward site for people who want to send short notifications by accessing a URL.
 			</Typography>
+			<br />
+			<Typography>
+				There's no need to write any code. Create an account, create a new message with a destination phone number and click the link to send the message. Piece of cake 🍰!
+			</Typography>
+			<br />
+			<center><img src="/landing.png" height="500px" border="1px solid"></img></center>
 		</Page>
 	);
 };
